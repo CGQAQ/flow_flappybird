b6a46d423613b0d30dafc8fa5c7acc7ae6b35380
Jason <m.jason.liu@outlook.com>
2018-07-25 02:12:54 +0800

    bird&land

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,6 @@ class Point {
     }
 }
 
-
 class Vector {
     x: number;
     y: number;
@@ -53,9 +52,7 @@ class Vector {
     }
 
     // never use;
-    crossProductOf(v: Vector) {
-
-    }
+    crossProductOf(v: Vector) {}
 }
 
 class Direction {
@@ -63,7 +60,7 @@ class Direction {
     static readonly Right = new Vector(1, 0);
     static readonly Down = new Vector(0, 1);
     static readonly Left = new Vector(-1, 0);
-    private constructor() { }
+    private constructor() {}
 }
 
 export class Sprite {
@@ -75,30 +72,30 @@ export class Sprite {
     height: number;
     destW: number;
     destH: number;
-    constructor(x: number,
+    constructor(
+        x: number,
         y: number,
         frames: HTMLImageElement[] | HTMLImageElement,
         frameCount?: number,
         destW?: number,
-        destH?: number,
+        destH?: number
     ) {
         if (frames === undefined) {
             this.frames = [];
-        }
-        else {
+        } else {
             this.frames = frames;
             if (!(this.frames instanceof Array) && frameCount !== undefined) {
                 this.frameCount = frameCount;
                 this.width = this.frames.width;
                 this.height = this.frames.height / this.frameCount;
-            }
-            else if (this.frames instanceof Array) {
+            } else if (this.frames instanceof Array) {
                 this.frameCount = this.frames.length;
                 this.width = this.frames[0].width;
                 this.height = this.frames[0].height;
-            }
-            else {
-                throw new Error('Frames are composed in one single element and didn\'t providing frame count!');
+            } else {
+                throw new Error(
+                    "Frames are composed in one single element and didn't providing frame count!"
+                );
             }
         }
 
@@ -112,8 +109,7 @@ export class Sprite {
         if (this.frames instanceof Array) {
             this.frames.push(frame);
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
@@ -122,17 +118,14 @@ export class Sprite {
         if (this.frames instanceof Array) {
             if (this.curr + 1 < this.frameCount) {
                 return this.frames[this.curr++];
+            } else {
+                return this.frames[(this.curr = 0)];
             }
-            else {
-                return this.frames[this.curr = 0];
-            }
-        }
-        else {
+        } else {
             // there is img element, handle by yourself.
             if (this.curr + 1 < this.frameCount) {
                 return [this.frames, this.curr++];
-            }
-            else {
+            } else {
                 const curr = this.curr;
                 this.curr = 0;
                 return [this.frames, curr];
@@ -147,26 +140,24 @@ export class Sprite {
     draw(ctx: CanvasRenderingContext2D) {
         // only work for frames in different element
         if (this.frames instanceof Array) {
-
             // @ts-ignores
             ctx.drawImage(this.getCurr(), this.pos.x, this.pos.y);
-        }
-        else {
+        } else {
             // @ts-ignore
             const [img, curr] = this.getCurr();
             // console.log(this.width, this.height)
             // @ts-ignore
-            ctx.drawImage(img,
+            ctx.drawImage(
+                // @ts-ignore
+                img,
                 0,
                 // @ts-ignore
-                curr * this.height,
-                // @ts-ignore
-                this.width, this.height,
-                // @ts-ignore
-                this.pos.x, this.pos.y,
-                // @ts-ignore
-                this.destW || this.width,
-                // @ts-ignore
+                curr * this.height, // @ts-ignore
+                this.width,
+                this.height, // @ts-ignore
+                this.pos.x,
+                this.pos.y, // @ts-ignore
+                this.destW || this.width, // @ts-ignore
                 this.destH || this.height
             );
         }
@@ -196,75 +187,178 @@ export interface Images {
     medal: Array<HTMLImageElement>;
 }
 
-export class Texture {
-    move() { }
+interface Moveable {
+    move(v: Vector);
 }
 
-// bird
-export class Bird extends Sprite {
-    constructor(x: number,
+export class Texture {
+    img: HTMLImageElement;
+    pos: Point;
+    constructor(
+        img: HTMLImageElement,
+        x: number,
         y: number,
-        img: HTMLImageElement | HTMLImageElement[],
-        frameCount?: number) {
-        super(x, y, img, frameCount);
+        dx: number,
+        dy: number
+    ) {
+        this.img = img;
+        this.pos = new Point(dx, dy);
+    }
+    move(v: Vector) {
+        this.pos.apply(v);
     }
 }
 
-// export class;
+// bird
+export class Bird extends Sprite implements Moveable {
+    speed: Vector;
+    maxy: number;
+    constructor(
+        x: number,
+        y: number,
+        maxy: number,
+        img: HTMLImageElement | HTMLImageElement[],
+        frameCount?: number
+    ) {
+        // ground
+        super(x, y, img, frameCount);
 
+        // default is still
+        this.speed = new Vector(0, 0);
+
+        this.maxy = maxy;
+    }
+
+    // apply a acceleration to bird, such as gravity
+    apply(v: Vector) {
+        this.speed.plus(v);
+        if (this.speed.y < -10) {
+            this.speed.y = -10;
+        }
+        if (this.pos.y + this.height > this.maxy && this.speed.y > 0) {
+            this.speed.scale(0);
+        } else if (this.pos.y < 0 && this.speed.y < 0) {
+            this.speed.scale(0);
+        }
+        if (this.pos) this.pos.apply(this.speed.scale(0.98));
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        super.draw(ctx);
+    }
+}
+
+export class Land extends Texture implements Moveable {
+    offset: number;
+
+    constructor(img) {
+        super(img, 0, 0, 0, 0);
+        this.offset = 0;
+    }
+
+    move(v: Vector) {
+        this.pos.apply(v);
+    }
+
+    draw(ctx: CanvasRenderingContext2D, width, height) {
+        for (let base = 0; base <= this.img.width * 2; base += this.img.width) {
+            ctx.drawImage(
+                this.img,
+                0,
+                0,
+                this.img.width,
+                this.img.height,
+                base + this.offset,
+                height - this.img.height,
+                this.img.width,
+                this.img.height
+            );
+        }
+        this.offset--;
+        if (-this.offset > this.img.width) {
+            this.offset = 0;
+        }
+    }
+}
 
 // pipe
 class Pipe {
     x: number;
     y: number;
-    constructor() {
-
-    }
+    constructor() {}
 }
-
-
 
 export class Game {
     width: number;
     height: number;
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
+    gravity: Vector;
+    initSpeed: Vector;
 
     images: Images;
 
     // will render
     bird: Bird;
-    land: Sprite;
+    land: Land;
 
-    constructor(canvas: HTMLCanvasElement, images: Images, width: number = Width, height: number = Height) {
+    constructor(
+        canvas: HTMLCanvasElement,
+        images: Images,
+        width: number = Width,
+        height: number = Height
+    ) {
         this.width = width;
         this.height = height;
         this.canvas = canvas;
-        this.ctx = canvas.getContext('2d');
-        this.ctx.fillStyle = 'white';
-        this.resize(width, height)
+        this.canvas.onmousedown = this.onMouseDown.bind(this);
+        this.ctx = canvas.getContext("2d");
+        this.ctx.fillStyle = "white";
+        this.gravity = new Vector(0, 0.45);
+        this.initSpeed = new Vector(0, 0);
+        this.resize(width, height);
         this.images = images;
-        this.bird = new Bird(0, 0, this.images.bird, 4);
-        this.mainloop()
+        this.bird = new Bird(
+            this.width / 8,
+            this.height / 2,
+            this.height - this.images.land.height,
+            this.images.bird,
+            4
+        );
+        this.land = new Land(this.images.land);
+        this.mainloop();
     }
 
     resize(width: number, height: number) {
         this.width = width;
         this.height = height;
-        this.canvas.setAttribute('width', width + 'px');
-        this.canvas.setAttribute('height', height + 'px');
+        this.canvas.setAttribute("width", width + "px");
+        this.canvas.setAttribute("height", height + "px");
     }
 
     mainloop() {
-        requestAnimationFrame(this.render.bind(this));
-        // this.bird.move()
-        setTimeout(this.mainloop.bind(this), 100);
+        setTimeout(this.mainloop.bind(this), 500);
     }
 
     render() {
         this.ctx.clearRect(0, 0, this.width, this.height);
+
+        // gravity
+        this.bird.apply(this.gravity);
+
         this.bird.draw(this.ctx);
+        this.land.draw(this.ctx, this.width, this.height);
         // this.bird.move(Direction.Right)
         // this.land.draw(this.ctx)
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    onMouseDown(e) {
+        if (e.which == 1) {
+            // left button clicked
+            this.bird.apply(new Vector(0, -15));
+            console.log(this.bird.speed);
+        }
     }
 }
