6aede74467eb6504ebc5179be95fd75c5f069d61
Jason <m.jason.liu@outlook.com>
2018-07-25 13:08:15 +0800

    basically completed

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,58 @@
 import $ from "jquery";
-import { Game, Bird, Sprite, Images } from './main';
+import { Game, Bird, Sprite, Images, Sounds } from "./main";
 
-
-$(document).ready(function () {
-    const game_canvas: $<HTMLCanvasElement> = $('#game-canvas');
-    const bird = $('.bird');
-    const ceiling = $('.ceiling');
-    const land = $('.land');
-    const replay = $('.replay');
-    const scoreboard = $('.scoreboard');
-    const sky = $('.sky');
-    const splash = $('.splash');
-    const thumb = $('.thumb');
+$(document).ready(function() {
+    const game_canvas: $<HTMLCanvasElement> = $("#game-canvas");
+    const bird = $(".bird");
+    const ceiling = $(".ceiling");
+    const land = $(".land");
+    const replay = $(".replay");
+    const scoreboard = $(".scoreboard");
+    const sky = $(".sky");
+    const splash = $(".splash");
+    const thumb = $(".thumb");
 
     // big font;
-    const font_big_0 = $('.font-big-0');
-    const font_big_1 = $('.font-big-1');
-    const font_big_2 = $('.font-big-2');
-    const font_big_3 = $('.font-big-3');
-    const font_big_4 = $('.font-big-4');
-    const font_big_5 = $('.font-big-5');
-    const font_big_6 = $('.font-big-6');
-    const font_big_7 = $('.font-big-7');
-    const font_big_8 = $('.font-big-8');
-    const font_big_9 = $('.font-big-9');
+    const font_big_0 = $(".font-big-0");
+    const font_big_1 = $(".font-big-1");
+    const font_big_2 = $(".font-big-2");
+    const font_big_3 = $(".font-big-3");
+    const font_big_4 = $(".font-big-4");
+    const font_big_5 = $(".font-big-5");
+    const font_big_6 = $(".font-big-6");
+    const font_big_7 = $(".font-big-7");
+    const font_big_8 = $(".font-big-8");
+    const font_big_9 = $(".font-big-9");
 
     // small font
-    const font_small_0 = $('.font-small-0');
-    const font_small_1 = $('.font-small-1');
-    const font_small_2 = $('.font-small-2');
-    const font_small_3 = $('.font-small-3');
-    const font_small_4 = $('.font-small-4');
-    const font_small_5 = $('.font-small-5');
-    const font_small_6 = $('.font-small-6');
-    const font_small_7 = $('.font-small-7');
-    const font_small_8 = $('.font-small-8');
-    const font_small_9 = $('.font-small-9');
+    const font_small_0 = $(".font-small-0");
+    const font_small_1 = $(".font-small-1");
+    const font_small_2 = $(".font-small-2");
+    const font_small_3 = $(".font-small-3");
+    const font_small_4 = $(".font-small-4");
+    const font_small_5 = $(".font-small-5");
+    const font_small_6 = $(".font-small-6");
+    const font_small_7 = $(".font-small-7");
+    const font_small_8 = $(".font-small-8");
+    const font_small_9 = $(".font-small-9");
 
     // medal
-    const medal_bronze = $('.medal-bronze');
-    const medal_gold = $('.medal-gold');
-    const medal_platinum = $('.medal-platinum');
-    const medal_silver = $('.medal-silver');
+    const medal_bronze = $(".medal-bronze");
+    const medal_gold = $(".medal-gold");
+    const medal_platinum = $(".medal-platinum");
+    const medal_silver = $(".medal-silver");
 
     // pipe
-    const pipe = $('.pipe');
-    const pipe_up = $('.pipe-up');
-    const pipe_down = $('.pipe-down');
+    const pipe = $(".pipe");
+    const pipe_up = $(".pipe-up");
+    const pipe_down = $(".pipe-down");
+
+    // sounds
+    const sfx_wing = $(".sfx-wing").get(0);
+    const sfx_point = $(".sfx-point").get(0);
+    const sfx_hit = $(".sfx-hit").get(0);
+    const sfx_die = $(".sfx-die").get(0);
+    const sfx_swooshing = $(".sfx-swooshing").get(0);
 
     const imgs: Images = {
         bird: bird.get(0),
@@ -57,6 +63,7 @@ $(document).ready(function () {
         sky: sky.get(0),
         splash: splash.get(0),
         thumb: thumb.get(0),
+        pipe: [pipe.get(0), pipe_up.get(0), pipe_down.get(0)],
         font_big: [
             font_big_0.get(0),
             font_big_1.get(0),
@@ -67,7 +74,7 @@ $(document).ready(function () {
             font_big_6.get(0),
             font_big_7.get(0),
             font_big_8.get(0),
-            font_big_9.get(0),
+            font_big_9.get(0)
         ],
         font_small: [
             font_small_0.get(0),
@@ -79,27 +86,31 @@ $(document).ready(function () {
             font_small_6.get(0),
             font_small_7.get(0),
             font_small_8.get(0),
-            font_small_9.get(0),
+            font_small_9.get(0)
         ],
         medal: [
             medal_bronze.get(0),
             medal_gold.get(0),
             medal_platinum.get(0),
-            medal_silver.get(0),
-        ],
-    }
+            medal_silver.get(0)
+        ]
+    };
 
+    const sounds: Sounds = {
+        sfx_wing,
+        sfx_point,
+        sfx_hit,
+        sfx_die,
+        sfx_swooshing
+    };
 
-
-
-    const game = new Game(game_canvas.get(0), imgs);
+    const game = new Game(game_canvas.get(0), imgs, sounds);
 
     // const s = new Spirit(0, 0);
     // s.addFrame(bird);
     // s.draw(game.ctx);
 
-
-    const ctx = game_canvas.get(0).getContext('2d');
+    const ctx = game_canvas.get(0).getContext("2d");
     // ctx.drawImage(bird.get(0), 0, 0)
     // ctx.drawImage(bird.get(0), 0, 0)
     // const s = new Sprite(0, 0, bird.get(0), 4);
@@ -108,5 +119,5 @@ $(document).ready(function () {
     //     requestAnimationFrame(render)
     // }
     // requestAnimationFrame(render);
-    game.render()
+    game.render();
 });
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 const Width = 600;
 const Height = 700;
 
+const MinimumGap = 100;
+const MinimumSpace = 70;
+const MaximumSpace = 200;
+
 // position
 class Point {
     x: number;
@@ -179,12 +183,22 @@ export interface Images {
     sky: HTMLImageElement;
     splash: HTMLImageElement;
     thumb: HTMLImageElement;
+    // pipes
+    pipe: Array<HTMLImageElement>;
     // big fonts
     font_big: Array<HTMLImageElement>;
     // small fonts
     font_small: Array<HTMLImageElement>;
     // medals
     medal: Array<HTMLImageElement>;
+}
+
+export interface Sounds {
+    sfx_wing: HTMLAudioElement;
+    sfx_point: HTMLAudioElement;
+    sfx_hit: HTMLAudioElement;
+    sfx_die: HTMLAudioElement;
+    sfx_swooshing: HTMLAudioElement;
 }
 
 interface Moveable {
@@ -211,11 +225,14 @@ export class Texture {
 
 // bird
 export class Bird extends Sprite implements Moveable {
+    frames: HTMLImageElement;
     speed: Vector;
+    miny: number;
     maxy: number;
     constructor(
         x: number,
         y: number,
+        miny: number,
         maxy: number,
         img: HTMLImageElement | HTMLImageElement[],
         frameCount?: number
@@ -225,7 +242,7 @@ export class Bird extends Sprite implements Moveable {
 
         // default is still
         this.speed = new Vector(0, 0);
-
+        this.miny = miny;
         this.maxy = maxy;
     }
 
@@ -237,10 +254,19 @@ export class Bird extends Sprite implements Moveable {
         }
         if (this.pos.y + this.height > this.maxy && this.speed.y > 0) {
             this.speed.scale(0);
-        } else if (this.pos.y < 0 && this.speed.y < 0) {
+        } else if (this.pos.y < this.miny && this.speed.y < 0) {
             this.speed.scale(0);
         }
-        if (this.pos) this.pos.apply(this.speed.scale(0.98));
+        if (this.pos) this.pos.apply(this.speed.scale(0.96));
+    }
+
+    die(maxy: number) {
+        if (this.pos.y + this.frames.height/4 >= maxy) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     draw(ctx: CanvasRenderingContext2D) {
@@ -260,8 +286,12 @@ export class Land extends Texture implements Moveable {
         this.pos.apply(v);
     }
 
-    draw(ctx: CanvasRenderingContext2D, width, height) {
-        for (let base = 0; base <= this.img.width * 2; base += this.img.width) {
+    draw(ctx: CanvasRenderingContext2D, width: number, height: number) {
+        for (
+            let base = 0;
+            base <= this.img.width * (width / this.img.width + 1);
+            base += this.img.width
+        ) {
             ctx.drawImage(
                 this.img,
                 0,
@@ -281,52 +311,284 @@ export class Land extends Texture implements Moveable {
     }
 }
 
+export class Sky extends Texture implements Moveable {
+    offset: number;
+
+    constructor(img) {
+        super(img, 0, 0, 0, 0);
+        this.offset = 0;
+    }
+
+    move(v: Vector) {
+        this.pos.apply(v);
+    }
+
+    draw(
+        ctx: CanvasRenderingContext2D,
+        y: number,
+        width: number,
+        height: number
+    ) {
+        for (let base = 0; base < width * 2; base += width) {
+            ctx.drawImage(
+                this.img,
+                0,
+                0,
+                this.img.width,
+                this.img.height,
+                base + this.offset,
+                y,
+                width,
+                height
+            );
+        }
+        this.offset--;
+        if (-this.offset > width) {
+            this.offset = 0;
+        }
+    }
+}
+
+export class Ceiling extends Texture {
+    offset: number;
+
+    constructor(img) {
+        super(img, 0, 0, 0, 0);
+        this.offset = 0;
+    }
+
+    move(v: Vector) {
+        this.pos.apply(v);
+    }
+
+
+    draw(ctx: CanvasRenderingContext2D, width, height) {
+        for (
+            let base = 0;
+            base <= this.img.width * (width / this.img.width + 1);
+            base += this.img.width
+        ) {
+            ctx.drawImage(
+                this.img,
+                0,
+                0,
+                this.img.width,
+                this.img.height,
+                base + this.offset,
+                0,
+                this.img.width,
+                this.img.height
+            );
+        }
+        this.offset--;
+        if (-this.offset > this.img.width) {
+            this.offset = 0;
+        }
+    }
+}
+
 // pipe
-class Pipe {
+abstract class Pipe {
+    readonly name
     x: number;
     y: number;
-    constructor() {}
+    height: number;
+    top: HTMLImageElement;
+    body: HTMLImageElement;
+
+    constructor(
+        top: HTMLImageElement,
+        body: HTMLImageElement,
+        x: number,
+        y: number,
+        height: number
+    ) {
+        this.x = x;
+        this.y = y;
+        this.top = top;
+        this.body = body;
+        this.height = height;
+    }
+    move() {
+        this.x--;
+    }
+
+    hit(bird: Bird): boolean {
+        if (this.name == "up pipe" && bird.pos.x + bird.frames.width > this.x && bird.pos.x < this.x + this.top.width && bird.pos.y + bird.frames.height / 4 > this.y) {
+            return true;
+        } else if (this.name == "down pipe" && bird.pos.x + bird.frames.width > this.x && bird.pos.x < this.x + this.top.width && bird.pos.y < this.y + this.height) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    score(bird: Bird): boolean {
+        if (this.x + this.top.width/2 == bird.pos.x + bird.frames.width) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    offscreen(width: number): boolean {
+        if (this.x < -width) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    abstract draw(ctx: CanvasRenderingContext2D);
+}
+
+class TopPipe extends Pipe {
+    readonly name = "down pipe";
+    constructor(
+        top: HTMLImageElement,
+        body: HTMLImageElement,
+        x: number,
+        y: number,
+        height: number
+    ) {
+        super(top, body, x, y, height);
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.drawImage(
+            this.body,
+            0,
+            0,
+            this.body.width,
+            this.body.height,
+            this.x,
+            this.y,
+            this.body.width,
+            this.height - this.top.height
+        );
+        ctx.drawImage(
+            this.top,
+            0,
+            0,
+            this.top.width,
+            this.top.height,
+            this.x,
+            this.y + (this.height - this.top.height),
+            this.top.width,
+            this.top.height
+        );
+    }
+}
+
+class BottomPipe extends Pipe {
+    readonly name = "up pipe";
+    constructor(
+        top: HTMLImageElement,
+        body: HTMLImageElement,
+        x: number,
+        y: number,
+        height: number
+    ) {
+        super(top, body, x, y, height);
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.drawImage(
+            this.top,
+            0,
+            0,
+            this.top.width,
+            this.top.height,
+            this.x,
+            this.y,
+            this.top.width,
+            this.top.height
+        );
+        ctx.drawImage(
+            this.body,
+            0,
+            0,
+            this.body.width,
+            this.body.height,
+            this.x,
+            this.y + this.top.height,
+            this.body.width,
+            this.height - this.top.height
+        );
+    }
+}
+
+
+enum GameState{
+    Start,
+    Stop,
+    Pause,
 }
 
 export class Game {
+
+    state: GameState;
+    score: number;
+
     width: number;
     height: number;
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
+    fcount: number;
+    fps: number;
     gravity: Vector;
     initSpeed: Vector;
 
     images: Images;
+    sounds: Sounds;
 
     // will render
     bird: Bird;
     land: Land;
+    ceiling: Ceiling;
+    sky: Sky;
+    pipes: Pipe[];
 
     constructor(
         canvas: HTMLCanvasElement,
         images: Images,
+        sounds: Sounds,
         width: number = Width,
         height: number = Height
     ) {
         this.width = width;
         this.height = height;
         this.canvas = canvas;
+        this.fcount = 0;
+        this.fps = 0;
         this.canvas.onmousedown = this.onMouseDown.bind(this);
         this.ctx = canvas.getContext("2d");
         this.ctx.fillStyle = "white";
-        this.gravity = new Vector(0, 0.45);
-        this.initSpeed = new Vector(0, 0);
         this.resize(width, height);
         this.images = images;
+        this.sounds = sounds;
+        this.init();
+        this.mainloop();
+    }
+
+    init() {
+        this.state = GameState.Stop;
+        this.score = 0;
+        this.gravity = new Vector(0, 0.45);
+        this.initSpeed = new Vector(0, 0);
         this.bird = new Bird(
             this.width / 8,
             this.height / 2,
+            this.images.ceiling.height,
             this.height - this.images.land.height,
             this.images.bird,
             4
         );
         this.land = new Land(this.images.land);
-        this.mainloop();
+        this.ceiling = new Ceiling(this.images.ceiling);
+        this.sky = new Sky(this.images.sky);
+        this.pipes = [];
     }
 
     resize(width: number, height: number) {
@@ -337,28 +599,176 @@ export class Game {
     }
 
     mainloop() {
-        setTimeout(this.mainloop.bind(this), 500);
+        if (this.state == GameState.Start) {
+            this.fps = this.fcount;
+            this.fcount = 0;
+            this.pipes.push(...this.generate_pipe());
+            this.pipes.forEach((pipe, index) => {
+                if (pipe.offscreen(this.width)) {
+                    this.pipes.slice(index, 1);
+                }
+            });
+        }
+        
+        setTimeout(this.mainloop.bind(this), 1000);
+    }
+
+    generate_pipe(): [Pipe, Pipe] {
+        let gap =
+            (Math.floor(Math.random() * 10000) %
+                (Math.max(
+                    this.height / 3,
+                    this.images.ceiling.height + this.images.land.height
+                ) -
+                    MinimumGap)) +
+            MinimumGap;
+        const lastX =
+            this.pipes.length > 0 ? this.pipes[this.pipes.length - 1].x : 0;
+        const lastY =
+            this.pipes.length > 0 ? this.pipes[this.pipes.length - 1].y : 0;
+        const lastGap =
+            this.pipes.length > 0
+                ? this.pipes[this.pipes.length - 1].y -
+                  this.pipes[this.pipes.length - 2].y
+                : 0;
+        const lastCy = lastY - lastGap / 2;
+        let x = 0;
+        if (this.pipes.length <= 0) {
+            x = this.width / 2 * 1.5;
+        }
+        else{
+            x = (Math.floor(Math.random() * 10000) %
+                (MaximumSpace - MinimumSpace)) +
+                MinimumSpace +
+                lastX + this.images.pipe[1].width;
+        }
+        let cy = 0;
+        if (x - lastX > this.images.bird.width * 2.5 || lastX == 0) {
+            cy =
+                (Math.floor(Math.random() * 10000) %
+                    (this.height -
+                        this.images.land.height -
+                        this.images.pipe[2].height -
+                        gap / 2 -
+                        (this.images.ceiling.height +
+                            this.images.pipe[1].height +
+                            gap / 2))) +
+                (this.images.ceiling.height +
+                    this.images.pipe[1].height +
+                    gap / 2);
+        } else {
+            const r =
+                (Math.floor(Math.random() * 10000) % lastGap) - lastGap / 2;
+            cy = lastCy + r;
+            console.log(r, lastGap / 2, lastCy);
+            if (
+                cy - gap <
+                this.images.ceiling.height + this.images.pipe[2].height
+            ) {
+                cy =
+                    this.images.ceiling.height +
+                    this.images.pipe[2].height +
+                    gap;
+            } else if (
+                cy - gap >
+                this.height -
+                    this.images.land.height -
+                    this.images.pipe[1].height
+            ) {
+                cy =
+                    this.height -
+                    this.images.land.height -
+                    this.images.pipe[1].height -
+                    gap;
+            }
+        }
+
+        return [
+            new TopPipe(
+                this.images.pipe[2],
+                this.images.pipe[0],
+                x,
+                this.images.ceiling.height,
+                cy - gap / 2 - this.images.ceiling.height
+            ),
+            new BottomPipe(
+                this.images.pipe[1],
+                this.images.pipe[0],
+                x,
+                cy + gap / 2,
+                this.height - (cy + gap / 2) - this.images.land.height
+            )
+        ];
     }
 
     render() {
         this.ctx.clearRect(0, 0, this.width, this.height);
 
-        // gravity
-        this.bird.apply(this.gravity);
-
-        this.bird.draw(this.ctx);
+        this.sky.draw(
+            this.ctx,
+            this.images.ceiling.height,
+            this.width,
+            this.height - this.images.ceiling.height - this.images.land.height
+        );
         this.land.draw(this.ctx, this.width, this.height);
-        // this.bird.move(Direction.Right)
-        // this.land.draw(this.ctx)
+        this.ceiling.draw(this.ctx, this.width, this.height);
 
+        // gravity
+        if(this.state == GameState.Start)
+        this.bird.apply(this.gravity);
+        this.bird.draw(this.ctx);
+        if (this.bird.die(this.height - this.images.land.height))
+        {
+            this.playsfx(this.sounds.sfx_die);
+            this.playsfx(this.sounds.sfx_hit);
+            this.init();
+        }
+        
+
+        this.pipes.forEach(pipe => {
+            if (this.state == GameState.Start)
+            pipe.move();
+            pipe.draw(this.ctx);
+            if (pipe.hit(this.bird)) {
+                this.playsfx(this.sounds.sfx_hit);
+                this.init();
+            }
+            if (pipe.name == "up pipe" && pipe.score(this.bird)) {
+                this.playsfx(this.sounds.sfx_point);
+                this.score++;
+            }
+        });
+
+        // fps info
+        this.fcount++;
+        this.ctx.fillStyle = "black";
+        this.ctx.fillText(
+            `FPS: ${this.fps}`,
+            this.width - 50,
+            this.images.ceiling.height * 2
+        );
+        this.ctx.fillText(
+            `Score: ${this.score}`,
+            this.width - 50,
+            this.images.ceiling.height * 3
+        );
         requestAnimationFrame(this.render.bind(this));
     }
 
     onMouseDown(e) {
         if (e.which == 1) {
             // left button clicked
-            this.bird.apply(new Vector(0, -15));
-            console.log(this.bird.speed);
+            if (this.state != GameState.Start) {
+                this.state = GameState.Start;
+            }
+            this.bird.apply(new Vector(0, -12));
+            this.playsfx(this.sounds.sfx_wing);
         }
+    }
+
+    playsfx(sound: HTMLAudioElement) {
+        sound.pause();
+        sound.currentTime = 0;
+        sound.play();
     }
 }
